0a5be03ccde31b095fc3582f4c5ab2db790561a8
kwhong95 <kwhong95@kakao.com>
2021-01-09 15:13:31 +0900

    Hompage Complete

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,7 +1,6 @@
-import React, { FC, Fragment } from 'react';
+import React, { FC, Fragment, useEffect } from 'react';
 import  HomeVidio  from '../../asserts/HomeVidio.mp4';
-import Link from '@material-ui/core/Link';
-
+ 
 import './style/style.css';
 
 interface HomePageProps {
@@ -11,34 +10,44 @@ interface HomePageProps {
 
 const HomePage: FC<HomePageProps> = ({ translate }) => {
 
+  useEffect(() => {
+    const menuToggle = document.querySelector('.toggle');
+    const showcase = document.querySelector('.showcase');
+  
+    menuToggle?.addEventListener('click', () => {
+      menuToggle?.classList.toggle('active');
+      showcase?.classList.toggle('active');
+    });
+  })
+
   return (
-  <Fragment>
+    <Fragment>
     <section className="showcase">
       <header>
         <h2 className="logo">Logo</h2>
         <div className="toggle"></div>
       </header>
-        <video muted loop autoPlay><source src={HomeVidio} ></source></video>
-        <div className="overlay"></div>
-        <div className="text">
-          <h2>Driving Mate</h2>
-          <h3>A Good Mate for a Driver</h3>
-          <p>I'll show you all the information on the vehicle you're curious about.</p>
-        </div>
-        <ul className="social">
-          <li><a href="#"><img alt="facebook" src="https://i.ibb.co/x7P24fL/facebook.png"/></a></li>
-          <li><a href="#"><img alt="instagram" src="https://i.ibb.co/ySwtH4B/instagram.png" /></a></li>
-        </ul>
+      <video src={HomeVidio} muted loop autoPlay></video>
+      <div className="overlay"></div>
+      <div className="text">
+        <h2>Driving Mate</h2> 
+        <h3>A Good Mate for a Driver</h3>
+        <p>I'll show you all the information on the vehicle you're curious about</p>
+      </div>
+      <ul className="social">
+        <li><a href="https://www.facebook.com/profile.php?id=100002481298079"><img alt="facebook" src="https://i.ibb.co/x7P24fL/facebook.png" /></a></li>
+        <li><a href="https://www.instagram.com/k.w_hong_922/"><img alt="instagram" src="https://i.ibb.co/ySwtH4B/instagram.png" /></a></li>
+      </ul>
     </section>
-
     <div className="menu">
       <ul>
-        <li><Link>Sign In</Link></li>
-        <li><Link>Sign Up</Link></li>
+        <li><a href="/signup">Sign Up</a></li>
+        <li><a href="/signin">Sign In</a></li>
       </ul>
     </div>
   </Fragment>
   )
 }
+
 
 export default HomePage;
