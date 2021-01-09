6a3a5c50d22520e276307d3045ba74e8d3c6ba2b
kwhong95 <kwhong95@kakao.com>
2021-01-09 16:49:19 +0900

    Update SignUp Page

--- src/components/pages/SignUp.tsx
+++ src/components/pages/SignUp.tsx
@@ -1,10 +1,9 @@
+import { Button, FormControl, Grid, InputAdornment, TextField, Typography } from '@material-ui/core';
+import { AccountCircle, LockRounded, Face} from '@material-ui/icons';
 import React, { FC, FormEvent, Fragment, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../../store';
 import { setError, signup } from '../../store/actions/authActions';
-
-import Button from '../UI/Button';
-import Input from '../UI/Input';
 import Message from '../UI/Message';
 
 interface SignupProps {
@@ -35,39 +34,79 @@ const Signup: FC<SignupProps> = ({ translate }) => {
 
 
   return (
-    <Fragment>
-      <section className="section">
-        <div className="container">
-          <h2 className="has-text-centered is-size-2 mb-3">{translate('sign up')}</h2>
-          <form className="form" onSubmit={submitHandler}>
+    <Fragment>   
+      <Grid container style={{ minHeight: "100vh", alignItems: "center"}}>
+        <Grid item xs={12} sm={6} justify="center">
+          <img
+            src="https://i.pinimg.com/564x/48/a8/54/48a8544c6dd6b96126533daf1b6fdbfc.jpg"
+            style={{ width: '100%', height: "100vh", objectFit: "cover" }}
+            alt="brand"
+          />
+        </Grid>
+        <Grid item xs={12} sm={6} alignItems="center" direction="column" justify="space-between" style={{ padding: 10 }}>
+          <div />
+          <div style={{ display: 'flex', flexDirection:'column', maxWidth: 400, minWidth: 300, marginLeft: 35 }}>
+            <Grid container justify="center">
+              <Typography style={{ width: '200' }}>Logo</Typography>
+            </Grid>
+            <FormControl>
             {error && <Message type="danger" msg={error} />}
-            <Input
-              name="firstName"
-              value={firstName}
-              onChange={(e) => setFirstName(e.currentTarget.value)}
-              placeholder={translate('first name')}
-              label={translate('first name')}
-            />
-            <Input
-              type="email"
-              name="email"
-              value={email}
-              onChange={(e) => setEmail(e.currentTarget.value)}
-              placeholder={translate('email address')}
-              label={translate('email address')}
-            />
-            <Input
-              type="password"
-              name="password"
-              value={password}
-              onChange={(e) => setPassword(e.currentTarget.value)}
-              placeholder={translate('password')}
-              label={translate('password')}
-            />
-            <Button text={loading ? translate('loading') : translate('sign up')} className="is-primary is-fullwidth mt-5" disabled={loading} />
-          </form>
-        </div>      
-      </section>
+              <TextField 
+                type="text"
+                label="FirstName" 
+                margin="normal" 
+                onChange={(e) => setFirstName(e.currentTarget.value)}
+                value={firstName}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <Face />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+              <TextField 
+                type="email"
+                label="Email" 
+                margin="normal" 
+                onChange={(e) => setEmail(e.currentTarget.value)}
+                value={email}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <AccountCircle />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+              <TextField 
+                type="password"
+                label="Password" 
+                margin="normal"
+                value={password}
+                onChange={(e) => setPassword(e.currentTarget.value)}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <LockRounded />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </FormControl>
+            <div style={{ height: 20 }} />
+            <Button 
+              color="primary" 
+              variant="contained" 
+              onClick={submitHandler} 
+              disabled={loading}
+            >
+              {loading ? "Loading" : "Sign Up"}
+            </Button>
+            <div style={{ height: 20 }} />
+          </div>
+        </Grid>
+      </Grid>
     </Fragment>
   )
 }
