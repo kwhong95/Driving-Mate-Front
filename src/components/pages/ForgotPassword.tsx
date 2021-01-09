47ee36d5dec66bbfc6eca4023ab99ae1a6590211
kwhong95 <kwhong95@kakao.com>
2021-01-09 16:56:24 +0900

    Update ForgotPassword Page

--- src/components/pages/ForgotPassword.tsx
+++ src/components/pages/ForgotPassword.tsx
@@ -1,10 +1,10 @@
+import { FormControl, Grid, InputAdornment, TextField, Typography, Button } from '@material-ui/core';
+import { AccountCircle } from '@material-ui/icons';
 import React, { FC, FormEvent, Fragment, useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../../store';
 import { sendPasswordResetEmail, setError, setSuccess } from '../../store/actions/authActions';
 
-import Button from '../UI/Button';
-import Input from '../UI/Input';
 import Message from '../UI/Message';
 
 interface ForgotPasswordProps {
@@ -40,27 +40,54 @@ const ForgotPassword: FC<ForgotPasswordProps> = ({ translate }) => {
     await dispatch(sendPasswordResetEmail(email, 'Email sent!'));
     setLoading(false);
   }
-
+  
   return (
-    <Fragment> 
-      <section className="section">
-        <div className="container">
-          <h2 className="has-text-centered is-size-2 mb-3">{translate('reset password')}</h2>
-          <form className="form" onSubmit={submitHandler}>
+    <Fragment>   
+      <Grid container style={{ minHeight: "100vh", alignItems: "center"}}>
+        <Grid item xs={12} sm={6} justify="center">
+          <img
+            src="https://i.pinimg.com/564x/48/a8/54/48a8544c6dd6b96126533daf1b6fdbfc.jpg"
+            style={{ width: '100%', height: "100vh", objectFit: "cover" }}
+            alt="brand"
+          />
+        </Grid>
+        <Grid item xs={12} sm={6} alignItems="center" direction="column" justify="space-between" style={{ padding: 10 }}>
+          <div />
+          <div style={{ display: 'flex', flexDirection:'column', maxWidth: 400, minWidth: 300, marginLeft: 35 }}>
+            <Grid container justify="center">
+              <Typography style={{ width: '200' }}>Logo</Typography>
+            </Grid>
+            <FormControl>
             {error && <Message type="danger" msg={error} />}
             {success && <Message type="success" msg={success} />}
-            <Input
-              type="email"
-              name="email"
-              value={email}
-              onChange={(e) => setEmail(e.currentTarget.value)}
-              placeholder={translate('email address')}
-              label={translate('email address')}
-            />
-            <Button text={loading ? translate('loading') : translate('send password reset email')} className="is-primary is-fullwidth mt-5" disabled={loading} />
-          </form>
-        </div>      
-      </section>
+              <TextField 
+                type="email"
+                label="Email" 
+                margin="normal" 
+                onChange={(e) => setEmail(e.currentTarget.value)}
+                value={email}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <AccountCircle />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </FormControl>
+            <div style={{ height: 20 }} />
+            <Button 
+              color="primary" 
+              variant="contained" 
+              onClick={submitHandler} 
+              disabled={loading}
+            >
+              {loading ? "Loading" : "Send Email"}
+            </Button>
+            <div style={{ height: 20 }} />
+          </div>
+        </Grid>
+      </Grid>
     </Fragment>
   )
 }
