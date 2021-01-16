d83238981db29a9e1d80dbb408e0d3823a7c38af
kwhong95 <kwhong95@kakao.com>
2021-01-16 23:37:37 +0900

    Creating Dashbord Layout

--- src/components/pages/Private/UserApp.tsx
+++ src/components/pages/Private/UserApp.tsx
@@ -3,13 +3,11 @@ import React, { FC } from 'react'
 import { ThemeProvider } from '@material-ui/core';
 import theme from './theme';
 import GlobalStyles from './components/GlobalStyles';
-import MainLayout from './layouts/MainLayout/MainLayout';
 
 const UserApp: FC = () => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <MainLayout />
     </ThemeProvider>
   )
 }
--- src/components/pages/Private/layouts/DashboardLayout/DashboardLayout.tsx
+++ src/components/pages/Private/layouts/DashboardLayout/DashboardLayout.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core';
+import NavBar from './NavBar';
+import TopBar from './TopBar';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    backgroundColor: theme.palette.background.default,
+    backgroundColor: theme.palette.background.dark,
     display: 'flex',
     height: '100%',
     overflow: 'hidden',
@@ -13,7 +15,10 @@ const useStyles = makeStyles((theme) => ({
     display: 'flex',
     flex: '1 1 auto',
     overflow: 'hidden',
-    paddingTop: 64
+    paddingTop: 64,
+    [theme.breakpoints.up('lg')]: {
+      paddingLeft: 256
+    }
   },
   contentContainer: {
     display: 'flex',
@@ -27,11 +32,17 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
-const MainLayout = () => {
+const DashboardLayout = () => {
   const classes = useStyles();
+  const [isMobileNavOpen, setMobileNavOpen] = useState(false);
 
   return (
     <div className={classes.root}>
+      <TopBar onMobileNavOpen={() => setMobileNavOpen(true)} />
+      <NavBar
+        onMobileClose={() => setMobileNavOpen(false)}
+        openMobile={isMobileNavOpen}
+      />
       <div className={classes.wrapper}>
         <div className={classes.contentContainer}>
           <div className={classes.content}>
@@ -42,4 +53,4 @@ const MainLayout = () => {
   );
 };
 
-export default MainLayout;
+export default DashboardLayout;
